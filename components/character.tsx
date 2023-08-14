6297581fd570e681ce7ac01d93ba41fd37a757e2
Dev Kaliper <gxlthz@gmail.com>
2023-08-14 15:05:17 -0500

    working

--- components/character.tsx
+++ components/character.tsx
@@ -1,5 +1,6 @@
 import {Card, CardHeader, CardBody, CardFooter} from "@nextui-org/card";
 import {Button} from "@nextui-org/button"
+import {Chip} from "@nextui-org/chip"
 import Image from "next/image";
 
 const Character = (props:any) => {
@@ -7,8 +8,7 @@ const Character = (props:any) => {
     return (
         <Card isFooterBlurred isBlurred className="w-[400px] h-[300px] col-span-12 sm:col-span-5">
         <CardHeader className="absolute z-10 top-1 flex-col items-start">
-          <p className="text-tiny text-white/60 uppercase font-bold">New</p>
-          <h4 className="text-black font-medium text-2xl">{character.Nombre}</h4>
+          <Chip color={character.Estado =="Vivo" ? "success" : character.Estado =="Fallecido" ? "danger" : "default"} variant="shadow">{character.Estado}</Chip>
         </CardHeader>
         <Image
         width={2000}
@@ -22,6 +22,7 @@ const Character = (props:any) => {
         <CardFooter className="absolute bg-white/30 bottom-0 border-t-1 border-zinc-100/50 z-10 justify-between">
           <div>
             <p className="text-black font-medium text-2xl">{character.Nombre}</p>
+            <p className="text-black font-semibold">{character.Ocupacion}</p>
            
           </div>
           <Button className="text-tiny" color="primary" radius="full" size="sm">
