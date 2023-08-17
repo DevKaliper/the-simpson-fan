7b9028765685f3561cd460eea3c8f35e29a3e03b
Dev Kaliper <gxlthz@gmail.com>
2023-08-16 20:20:43 -0500

    fetching quotes done

--- app/quotes/page.tsx
+++ app/quotes/page.tsx
@@ -1,8 +1,10 @@
 "use client";
 import Link from "next/link";
 import { getQuote } from "@/controllers/request";
-import { ReactNode, useEffect, useState } from "react";
+import { Skeleton } from "@nextui-org/skeleton";
+import { useEffect, useState } from "react";
 import Image from "next/image";
+import { Button } from "@nextui-org/button";
 
 type Quotes = {
   quote: string;
@@ -13,14 +15,36 @@ type Quotes = {
 
 export default function Home() {
   const [quote, setQuote] = useState<Quotes>();
+  const [loading, setLoading] = useState(true);
 
   const fetchingQuote = async () => {
+    setLoading(true);
     const response = await getQuote();
     setQuote(response);
+    setLoading(false);
   };
   useEffect(() => {
     fetchingQuote();
+    setTimeout(() => {
+      setLoading(false);
+    }, Math.floor(Math.random() * 5000));
   }, []);
+
+  if (loading) {
+    return (
+      <div className="min-w-screen flex h-[70vh] flex-col items-center justify-center gap-2">
+        <div className="flex w-full max-w-[300px] items-center gap-3">
+          <div>
+            <Skeleton className="flex h-20 w-20 rounded-full" />
+          </div>
+          <div className="flex w-full flex-col gap-2">
+            <Skeleton className="h-3 w-3/5 rounded-lg" />
+            <Skeleton className="h-3 w-4/5 rounded-lg" />
+          </div>
+        </div>
+      </div>
+    );
+  }
 
   return (
     <div className="min-w-screen flex h-[70vh] flex-col items-center justify-center gap-2">
@@ -38,6 +62,20 @@ export default function Home() {
           className="rounded-full"
         />
       )}
+
+      <div className="flex items-center justify-center gap-2 w-fit h-fit">
+        <Button
+          className="mt-4"
+          variant="shadow"
+          color="warning">
+          <Link href="/"> Volver</Link>
+        </Button>
+
+        <Button onPress={fetchingQuote} className="flex justify-center items-center mt-4"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99" />
+</svg> Otra
+</Button>
+      </div>
     </div>
   );
 }
